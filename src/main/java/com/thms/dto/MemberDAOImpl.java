93661bc50203fc94a96ab1085b84bb3263c974e4
yama <hyunny333@gmail.com>
2016-11-07 14:11:54 +0900

    login

---
+++ src/main/java/com/thms/dto/MemberDAOImpl.java
@@ -0,0 +1,21 @@
+package com.thms.dto;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.thms.domain.MemberVO;
+import com.thms.persistence.MemberDAO;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO {
+	@Inject
+	private SqlSession sqlSession;
+	private static String namespace = "com.thms.mapper.MemberMapper";
+
+	@Override
+	public MemberVO login(LoginDTO dto) throws Exception {
+		return sqlSession.selectOne(namespace +".login", dto);
+	}
+}
